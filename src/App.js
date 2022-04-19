217c46b436620d71d7c3dc7f5d687bdcd9443b28
mark-stan <mark-stanley@outlook.com>
2022-04-19 18:44:12 -0400

    testing tokens

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import tokens from './tokens';
+
+
 
 function App() {
-  return (
+const MyName = styled.h1`
+  color: ${tokens.global.Monza.value};
+`;
+
+return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MyName>Mark Stanley</MyName>
       </header>
     </div>
   );
